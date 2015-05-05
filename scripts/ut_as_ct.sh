81670681c631da7d09228efc2c2480f35a99ab37
zmshi <zmstone@163.com>
2015-05-05 06:34:05 -0400

    fix find issue

--- scripts/ut_as_ct.sh
+++ scripts/ut_as_ct.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # find all modules
-ERL_FILES="$(find ebin/*.beam -type f -printf "%f\0" | xargs -0 -I{} basename {} .beam)"
+ERL_FILES="$(find ebin/*.beam -type f -print0 | xargs -0 -I{} basename {} .beam)"
 MODULES="$(echo $ERL_FILES | tr ' ' ',')"
 
-# Erlang code find modules that has test/0 exported
+# Erlang code find modules having test/0 exported
 CODE="[io:format([io_lib:print(Module), $ ]) || Module <- [$MODULES], lists:member({test, 0}, Module:module_info(exports))]"
 
-# All modules has test/0 exported
+# Get all modules having test/0 exported
 UT_MODULES="$(erl -pa ebin -noshell -eval "$CODE" -s init stop)"
 
 cp test/gululog_ut_SUITE.erl.in test/gululog_ut_SUITE.erl
